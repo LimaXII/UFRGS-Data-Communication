86992a2d409e04642871537f5ad13c981921edd8
LimaXII <luccasdasilvalima@hotmail.com>
2025-01-04 21:14:52 -0300

    refactor

--- Grupo5_16QAM.m
+++ Grupo5_16QAM.m
@@ -1,3 +1,6 @@
+% -------------------------------------------------------------------------
+% Informações relevantes.
+% -------------------------------------------------------------------------
 % Trabalho Final - Grupo 5
 %
 % Especificações:
@@ -21,127 +24,139 @@
 % Thiago Leonel Rancan Bischoff - 00324856
 % -------------------------------------------------------------------------
 
-
+% -------------------------------------------------------------------------
+% Configurações e variáveis.
+% -------------------------------------------------------------------------
 clear;
 close;
-num_b = 1000000; % Número de bits a serem simulados (garanta que seja múltiplo de 4 para 16-QAM)
-frame_bits = 18400; % Número de bits por quadro (2300 bytes) para FER
-Eb_N0_dB = 0:1:9; % Faixa de Eb/N0
-Eb_N0_lin = 10 .^ (Eb_N0_dB / 10); % Eb/N0 linearizado
-ber = zeros(3, length(Eb_N0_lin)); % BER para as 3 versões
-fer = zeros(3, length(Eb_N0_lin)); % FER para as 3 versões
-Eb = 0.25; % Energia por bit para 16QAM
-NP = Eb ./ Eb_N0_lin; % Potência do ruído
-NA = sqrt(NP); % Amplitudes do ruído
+num_b = 1000000;                    % Número de bits a serem simulados.
+frame_bits = 18400;                 % Número de bits por quadro (2300 bytes) para FER.
+Eb_N0_dB = 0:1:9;                   % Faixa de Eb/N0.
+Eb_N0_lin = 10 .^ (Eb_N0_dB / 10);  % Eb/N0 linearizado.
+ber = zeros(3, length(Eb_N0_lin));  % BER para as 3 versões.
+fer = zeros(3, length(Eb_N0_lin));  % FER para as 3 versões.
+Eb = 0.25;                          % Energia por bit para 16QAM.
+NP = Eb ./ Eb_N0_lin;               % Potência do ruído.
+NA = sqrt(NP);                      % Amplitudes do ruído.
 
-%Configurações LDPC
-N = 1944;  % Tamanho do código
-R = 1/2;   % Taxa do código
-info_length = N * R;
+% -------------------------------------------------------------------------
+% Configurações do código LDPC
+% -------------------------------------------------------------------------
+N = 1944;                           % Tamanho do código LDPC.
+R = 1/2;                            % Taxa do código LDPC (1/2 significa que metade dos bits são redundantes).
+info_length = N * R;                % Comprimento dos bits de informação.
+    
+% -------------------------------------------------------------------------
+% Criação e carregamento do LDPC.
+% -------------------------------------------------------------------------
+aux_ldpc = LDPCCode(N, info_length); % Cria objeto de código LDPC.
+aux_ldpc.load_wifi_ldpc(N, R);       % Carregar o código LDPC correspondente ao H_1944_1_2.
+H = sparse(logical(aux_ldpc.H));     % Exibir a matriz H gerada.
 
-% Criar o objeto LDPC
-aux_ldpc = LDPCCode(N, info_length);
-
-% Carregar o código LDPC correspondente ao H_1944_1_2
-aux_ldpc.load_wifi_ldpc(N, R);
-
-% Exibir a matriz H gerada
-H = sparse(logical(aux_ldpc.H));
-
-% Inicialização do ldpc
+% Inicialização do LDPC.
 ldpcEncoder = comm.LDPCEncoder(H);
+% Decodificadores com decisão soft e hard.
 ldpcDecoderHard = comm.LDPCDecoder(H, 'DecisionMethod', 'Hard decision');
 ldpcDecoderSoft = comm.LDPCDecoder(H, 'DecisionMethod', 'Soft decision');
+% Modulação 16-QAM.
+mod_order = 16;                      % Ordem da modulação.
+symbols_per_bit = log2(mod_order);   % Número de bits por símbolo.
 
-% Modulação (16-QAM)
-mod_order = 16; % Ordem da modulação
-symbols_per_bit = log2(mod_order); % Número de bits por símbolo
+% -------------------------------------------------------------------------
+% Fonte de informação.
+% -------------------------------------------------------------------------
+data = randi([0 1], num_b, 1);                                  % Gera bits aleatórios.
+blocks = ceil(length(data) / info_length);                      % Número de blocos (iteração por info_length bits).
+data = [data; zeros(blocks * info_length - length(data), 1)];   % Ajusta o tamanho de 'data' para múltiplo de 'info_length' se necessário.
+data_reshaped = reshape(data, info_length, blocks);             % Reshape 'data' para que cada coluna tenha 'info_length' bits.
 
-% 1. Fonte de informação
-data = randi([0 1], num_b, 1); % Gera bits aleatórios
-blocks = ceil(length(data) / info_length); % Número de blocos (iteração por info_length bits)
-data = [data; zeros(blocks * info_length - length(data), 1)];% Ajusta o tamanho de 'data' para múltiplo de 'info_length' se necessário
-data_reshaped = reshape(data, info_length, blocks);% Reshape 'data' para que cada coluna tenha 'info_length' bits
-
+% -------------------------------------------------------------------------
+% Simulação.
+% -------------------------------------------------------------------------
+% Loop para diferentes valores de Eb/N0.
 for i = 1:length(Eb_N0_lin)
     num_bit_errors = zeros(3, 1);
     for j = 1:blocks
-        % 1. Seleciona o bloco atual de bits
-        bits = logical(data_reshaped(:, j)); % Pega o bloco de info_length bits
+        % 1. Seleciona o bloco atual de bits.
+        bits = logical(data_reshaped(:, j));    % Pega o bloco de info_length bits.
 
-        % 2. Codificação de canal LDPC
+        % 2. Codificação de canal LDPC.
         codedBits = step(ldpcEncoder, bits);
 
-        % 3. Modulação (BPSK com símbolos complexos)
-        % Agrupando bits para formar símbolos - Sem Codificação
-        num_symbols = length(bits) / symbols_per_bit; % Número de símbolos
-        tx_bits = reshape(bits, symbols_per_bit, num_symbols).'; % Reshape para vetor de símbolos
-        decimal_symbols = bi2de(tx_bits, 'left-msb'); % Conversão de bits para decimal
-        mod_symbols_uncode = qammod(decimal_symbols, mod_order); % Modulação 16-QAM (sem argumentos adicionais)
+        % 3. Modulação (BPSK com símbolos complexos).
+        % Agrupando bits para formar símbolos - Sem Codificação.
+        num_symbols = length(bits) / symbols_per_bit;               % Número de símbolos.
+        tx_bits = reshape(bits, symbols_per_bit, num_symbols).';    % Reshape para vetor de símbolos.
+        decimal_symbols = bi2de(tx_bits, 'left-msb');               % Conversão de bits para decimal.
+        mod_symbols_uncode = qammod(decimal_symbols, mod_order);    % Modulação 16-QAM (sem argumentos adicionais).
 
-        % Agrupando bits para formar símbolos - LDPC
-        num_symbols_ldpc = length(codedBits) / symbols_per_bit; % Número de símbolos
-        tx_bits_ldpc = reshape(codedBits, symbols_per_bit, num_symbols_ldpc).'; % Reshape para vetor de símbolos
-        decimal_symbols_ldpc = bi2de(tx_bits_ldpc, 'left-msb'); % Conversão de bits para decimal
-        mod_symbols_ldpc = qammod(decimal_symbols_ldpc, mod_order); % Modulação 16-QAM (sem argumentos adicionais)
+        % Agrupando bits para formar símbolos - LDPC.
+        num_symbols_ldpc = length(codedBits) / symbols_per_bit;                 % Número de símbolos.
+        tx_bits_ldpc = reshape(codedBits, symbols_per_bit, num_symbols_ldpc).'; % Reshape para vetor de símbolos.
+        decimal_symbols_ldpc = bi2de(tx_bits_ldpc, 'left-msb');                 % Conversão de bits para decimal.
+        mod_symbols_ldpc = qammod(decimal_symbols_ldpc, mod_order);             % Modulação 16-QAM (sem argumentos adicionais).
 
         % 4. Ruído AWGN (complexo)
         % Ruído AWGN (complexo) - Sem Código
         noise = NA(i) * (randn(size(mod_symbols_uncode)) + 1j * randn(size(mod_symbols_uncode)));
-        rx_symbols_uncode = mod_symbols_uncode + noise; % Sinal recebido sem ldpc
+        rx_symbols_uncode = mod_symbols_uncode + noise;                         % Sinal recebido sem código.
         
         % Ruído AWGN (complexo) - LDPC
         noise = NA(i) * (randn(size(mod_symbols_ldpc)) + 1j * randn(size(mod_symbols_ldpc)));
-        rx_symbols_ldpc = mod_symbols_ldpc + noise; % Sinal recebido sem ldpc
+        rx_symbols_ldpc = mod_symbols_ldpc + noise;                             % Sinal recebido sem LDPC.
 
         % 5. Demodulação
-        % Demodulação Sem Código
-        rx_decimal_symbols_uncode = qamdemod(rx_symbols_uncode, mod_order); % Demodulação 16-QAM
-        rx_bits_uncode = de2bi(rx_decimal_symbols_uncode, symbols_per_bit, 'left-msb').'; % Conversão de volta para bits
-        rx_bits_uncode = rx_bits_uncode(:); % Vetorizar os bits recebidos 
+        % Demodulação Sem Código.
+        rx_decimal_symbols_uncode = qamdemod(rx_symbols_uncode, mod_order);               % Demodulação 16-QAM.
+        rx_bits_uncode = de2bi(rx_decimal_symbols_uncode, symbols_per_bit, 'left-msb').'; % Conversão de volta para bits.
+        rx_bits_uncode = rx_bits_uncode(:);                                               % Vetorizar os bits recebidos.
         
-        % Demodulação LDPC
-        rx_decimal_symbols_ldpc = qamdemod(rx_symbols_ldpc, mod_order); % Demodulação 16-QAM
-        rx_bits_ldpc = de2bi(rx_decimal_symbols_ldpc, symbols_per_bit, 'left-msb').'; % Conversão de volta para bits
-        rx_bits_ldpc = 1 - (rx_bits_ldpc(:))*2; % Vetorizar os bits recebidos 
+        % Demodulação LDPC.
+        rx_decimal_symbols_ldpc = qamdemod(rx_symbols_ldpc, mod_order);                   % Demodulação 16-QAM.
+        rx_bits_ldpc = de2bi(rx_decimal_symbols_ldpc, symbols_per_bit, 'left-msb').';     % Conversão de volta para bits.
+        rx_bits_ldpc = 1 - (rx_bits_ldpc(:))*2;                                           % Vetorizar os bits recebidos.
         
-        % 6. Decodificação de canal
+        % 6. Decodificação de canal.
         decodedBitsHard = step(ldpcDecoderHard, rx_bits_ldpc);
         decodedBitsSoftllr = step(ldpcDecoderSoft, rx_bits_ldpc);
         decodedBitsSoft = decodedBitsSoftllr < -0.9;
 
         % 7. Cálculo de erros para as 3 versões
-        num_bit_errors(1) = num_bit_errors(1) + sum(bits ~= rx_bits_uncode); % Sem LDPC
-        num_bit_errors(2) = num_bit_errors(2) + sum(bits ~= decodedBitsHard); % LDPC Hard
-        num_bit_errors(3) = num_bit_errors(3) + sum(bits ~= decodedBitsSoft); % LDPC Soft
+        num_bit_errors(1) = num_bit_errors(1) + sum(bits ~= rx_bits_uncode);              % Sem LDPC.
+        num_bit_errors(2) = num_bit_errors(2) + sum(bits ~= decodedBitsHard);             % LDPC Hard.
+        num_bit_errors(3) = num_bit_errors(3) + sum(bits ~= decodedBitsSoft);             % LDPC Soft.
     end
-
-    display(num_bit_errors);
-    % 8. Calcula BER e FER
+    
+    % 8. Calcula BER e FER.
     ber(:, i) = num_bit_errors / (blocks * info_length);
-    fer(:, i) = 1 - (1 - ber(:, i)).^frame_bits; % Calculando o FER a partir do BER
+    fer(:, i) = 1 - (1 - ber(:, i)).^frame_bits;              % Calculando o FER a partir do BER
 end
 
-% 9. Plots de desempenho
+% -------------------------------------------------------------------------
+% Plots de desempenho.
+% -------------------------------------------------------------------------
+
+% Gráfico BER.
 figure;
-semilogy(Eb_N0_dB, ber(1, :), 'x-', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'Sem LDPC');
+semilogy(Eb_N0_dB, ber(1, :), 'r-x', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'Sem LDPC');  % Linha Vermelha.
 hold on;
-semilogy(Eb_N0_dB, ber(2, :), 'o-', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'LDPC Hard');
-semilogy(Eb_N0_dB, ber(3, :), 's-', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'LDPC Soft');
+semilogy(Eb_N0_dB, ber(2, :), 'g-o', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'LDPC Hard'); % Linha Verde.
+semilogy(Eb_N0_dB, ber(3, :), 'b-s', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'LDPC Soft'); % Linha Azul.
 xlabel('Eb/N0 (dB)');
 ylabel('BER');
 legend('Location', 'southwest');
 grid on;
-title('Desempenho do sistema BPSK com LDPC');
+title('Desempenho do sistema 16QAM com LDPC (BER)');
 
+% Gráfico FER.
 figure;
-semilogy(Eb_N0_dB, fer(1, :), 'x-', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'Sem LDPC');
+semilogy(Eb_N0_dB, fer(1, :), 'r-x', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'Sem LDPC');  % Linha Vermelha.
 hold on;
-semilogy(Eb_N0_dB, fer(2, :), 'o-', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'LDPC Hard');
-semilogy(Eb_N0_dB, fer(3, :), 's-', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'LDPC Soft');
+semilogy(Eb_N0_dB, fer(2, :), 'g-o', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'LDPC Hard'); % Linha Verde.
+semilogy(Eb_N0_dB, fer(3, :), 'b-s', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'LDPC Soft'); % Linha Azul.
 xlabel('Eb/N0 (dB)');
 ylabel('FER');
 legend('Location', 'southwest');
 grid on;
-title('Desempenho do sistema BPSK com LDPC (FER)');
+title('Desempenho do sistema 16QAM com LDPC (FER)');
 
--- Grupo5_BPSK.m
+++ Grupo5_BPSK.m
@@ -1,3 +1,6 @@
+% -------------------------------------------------------------------------
+% Informações relevantes.
+% -------------------------------------------------------------------------
 % Trabalho Final - Grupo 5
 %
 % Especificações:
@@ -20,111 +23,120 @@
 % Matheus Almeida Silva - 00316326
 % Thiago Leonel Rancan Bischoff - 00324856
 % -------------------------------------------------------------------------
+
+% -------------------------------------------------------------------------
+% Configurações e variáveis.
+% -------------------------------------------------------------------------
 clear;
 close;
+num_b = 1000000;        % Número de bits a serem simulados.
+frame_bits = 18400;     % Número de bits por quadro (2300 bytes) para FER.
+Eb_N0_dB = 0:1:9;       % Faixa de Eb/N0.
+Eb_N0_lin = 10 .^ (Eb_N0_dB / 10); % Eb/N0 linearizado.
+ber = zeros(3, length(Eb_N0_lin)); % BER para as 3 versões.
+fer = zeros(3, length(Eb_N0_lin)); % FER para as 3 versões.
+Eb = 1;                 % Energia por bit para BPSK.
+NP = Eb ./ Eb_N0_lin;   % Potência do ruído.
+NA = sqrt(NP);          % Amplitudes do ruído.
 
-% Configurações
-num_b = 1000000;
-frame_bits = 18400; % Número de bits por quadro (2300 bytes) para FER
-Eb_N0_dB = 0:1:9; % Faixa de Eb/N0
-Eb_N0_lin = 10 .^ (Eb_N0_dB / 10); % Eb/N0 linearizado
-ber = zeros(3, length(Eb_N0_lin)); % BER para as 3 versões
-fer = zeros(3, length(Eb_N0_lin)); % FER para as 3 versões
-Eb = 1; % Energia por bit para BPSK
-NP = Eb ./ Eb_N0_lin; % Potência do ruído
-NA = sqrt(NP); % Amplitudes do ruído
+% -------------------------------------------------------------------------
+% Configurações do código LDPC
+% -------------------------------------------------------------------------
+N = 1944;               % Tamanho do código LDPC.
+R = 1/2;                % Taxa do código LDPC (1/2 significa que metade dos bits são redundantes).
+info_length = N * R;    % Comprimento dos bits de informação.
 
-%Configurações LDPC
-N = 1944;  % Tamanho do código
-R = 1/2;   % Taxa do código
-info_length = N * R;
+% -------------------------------------------------------------------------
+% Criação e carregamento do LDPC.
+% -------------------------------------------------------------------------
+aux_ldpc = LDPCCode(N, info_length); % Cria objeto de código LDPC
+aux_ldpc.load_wifi_ldpc(N, R);       % Carrega parâmetros LDPC padrão (ex.: Wi-Fi)
+H = sparse(logical(aux_ldpc.H));     % Matriz H gerada.
 
-% Criar o objeto LDPC
-aux_ldpc = LDPCCode(N, info_length);
-
-% Carregar o código LDPC correspondente ao H_1944_1_2
-aux_ldpc.load_wifi_ldpc(N, R);
-
-% Exibir a matriz H gerada
-H = sparse(logical(aux_ldpc.H));
-
-%Inicialização do ldpc
-ldpcEncoder = comm.LDPCEncoder(H);
+% Inicialização do LDPC.
+ldpcEncoder = comm.LDPCEncoder(H);   % Codificador LDPC
+% Decodificadores com decisão soft e hard.
 ldpcDecoderHard = comm.LDPCDecoder(H, 'DecisionMethod', 'Hard decision');
 ldpcDecoderSoft = comm.LDPCDecoder(H, 'DecisionMethod', 'Soft decision');
-
-
-%Inicialização do BPSK mod e demod
+%Inicialização do BPSK mod e demod.
 pskDemodHard = comm.PSKDemodulator(2, 'BitOutput', true, 'DecisionMethod', 'Hard decision');
 
-% Fonte de informação
-data = randi([0 1], num_b, 1); % Gera bits aleatórios
-blocks = ceil(length(data) / info_length); % Número de blocos (iteração por info_length bits)
-data = [data; zeros(blocks * info_length - length(data), 1)];% Ajusta o tamanho de 'data' para múltiplo de 'info_length' se necessário
-data_reshaped = reshape(data, info_length, blocks);% Reshape 'data' para que cada coluna tenha 'info_length' bits
+% -------------------------------------------------------------------------
+% Fonte de informação.
+% -------------------------------------------------------------------------
+data = randi([0 1], num_b, 1);                                  % Gera bits aleatórios.
+blocks = ceil(length(data) / info_length);                      % Divide os bits em blocos de tamanho 'info_length'.
+data = [data; zeros(blocks * info_length - length(data), 1)];   % Ajusta comprimento para múltiplo de 'info_length'.
+data_reshaped = reshape(data, info_length, blocks);             % Reshape 'data' para que cada coluna tenha 'info_length' bits.
 
-% Simulação
-for i = 1:length(Eb_N0_lin)
+% -------------------------------------------------------------------------
+% Simulação.
+% -------------------------------------------------------------------------
+% Loop para diferentes valores de Eb/N0.
+for i = 1:length(Eb_N0_lin)                  
     num_bit_errors = zeros(3, 1);
     for j = 1:blocks
-        % 1. Seleciona o bloco atual de bits
-        bits = logical(data_reshaped(:, j)); % Pega o bloco de info_length bits
+        % 1. Seleciona o bloco atual de bits.
+        bits = logical(data_reshaped(:, j)); % Pega o bloco de info_length bits.
         
-        % 2. Codificação de canal (LDPC)
+        % 2. Codificação de canal (LDPC).
         codedBits = step(ldpcEncoder, bits);
 
-        % 3. Modulação (BPSK com símbolos complexos)
-        modulated_no_code = complex(2 * bits - 1, 0); % Sem LDPC
-        modulated_ldpc = complex(2 * codedBits - 1, 0); % Com LDPC
+        % 3. Modulação (BPSK com símbolos complexos).
+        modulated_no_code = complex(2 * bits - 1, 0);   % Sem LDPC.
+        modulated_ldpc = complex(2 * codedBits - 1, 0); % Com LDPC.
 
-        % 4. Ruído AWGN (complexo)
+        % 4. Ruído AWGN (complexo).
         noise_no_code = NA(i) * complex(randn(size(modulated_no_code)), randn(size(modulated_no_code))) * sqrt(0.5);
         noise_ldpc = NA(i) * complex(randn(size(modulated_ldpc)), randn(size(modulated_ldpc))) * sqrt(0.5);
 
         received_no_code = modulated_no_code + noise_no_code;
         received_ldpc = modulated_ldpc + noise_ldpc;
 
-        % 5. Demodulação
+        % 5. Demodulação.
         demod_no_code = (sign(real(received_no_code)) + 1) / 2;
         demod_ldpc = 1 - ((sign(real(received_ldpc)) + 1) / 2) * 2; 
         
-        % 6. Decodificação de canal
+        % 6. Decodificação de canal.
         decodedBitsHard = step(ldpcDecoderHard, demod_ldpc);
         decodedBitsSoftllr = step(ldpcDecoderSoft,  demod_ldpc);
         decodedBitsSoft = decodedBitsSoftllr < -0.9;
 
-        % 7. Cálculo de erros para as 3 versões
-        num_bit_errors(1) = num_bit_errors(1) + sum(bits ~= demod_no_code); % Sem LDPC
-        num_bit_errors(2) = num_bit_errors(2) + sum(bits ~= decodedBitsHard); % LDPC Hard
-        num_bit_errors(3) = num_bit_errors(3) + sum(bits ~= decodedBitsSoft); % LDPC Soft
+        % 7. Cálculo de erros para as 3 versões.
+        num_bit_errors(1) = num_bit_errors(1) + sum(bits ~= demod_no_code);   % Sem codificação.
+        num_bit_errors(2) = num_bit_errors(2) + sum(bits ~= decodedBitsHard); % LDPC Hard.
+        num_bit_errors(3) = num_bit_errors(3) + sum(bits ~= decodedBitsSoft); % LDPC Soft.
     end
 
-    display(num_bit_errors);
     % 8. Calcula BER e FER
     ber(:, i) = num_bit_errors / (j * info_length);
-    fer(:, i) = 1 - (1 - ber(:, i)).^frame_bits; % Calculando o FER a partir do BER
+    fer(:, i) = 1 - (1 - ber(:, i)).^frame_bits;    % Calculando o FER a partir do BER
 end
 
-% 9. Plots de desempenho
+% -------------------------------------------------------------------------
+% Plots de desempenho.
+% -------------------------------------------------------------------------
+
+% Gráfico BER.
 figure;
-semilogy(Eb_N0_dB, ber(1, :), 'x-', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'Sem LDPC');
+semilogy(Eb_N0_dB, ber(1, :), 'r-x', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'Sem LDPC'); % Linha Vermelha.
 hold on;
-semilogy(Eb_N0_dB, ber(2, :), 'o-', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'LDPC Hard');
-semilogy(Eb_N0_dB, ber(3, :), 's-', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'LDPC Soft');
+semilogy(Eb_N0_dB, ber(2, :), 'g-o', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'LDPC Hard'); % Linha Verde.
+semilogy(Eb_N0_dB, ber(3, :), 'b-s', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'LDPC Soft'); % Linha Azul.
 xlabel('Eb/N0 (dB)');
 ylabel('BER');
 legend('Location', 'southwest');
 grid on;
-title('Desempenho do sistema BPSK com LDPC');
+title('Desempenho do sistema BPSK com LDPC (BER)');
 
+% Gráfico FER.
 figure;
-semilogy(Eb_N0_dB, fer(1, :), 'x-', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'Sem LDPC');
+semilogy(Eb_N0_dB, fer(1, :), 'r-x', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'Sem LDPC'); % Linha Vermelha.
 hold on;
-semilogy(Eb_N0_dB, fer(2, :), 'o-', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'LDPC Hard');
-semilogy(Eb_N0_dB, fer(3, :), 's-', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'LDPC Soft');
+semilogy(Eb_N0_dB, fer(2, :), 'g-o', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'LDPC Hard'); % Linha Verde.
+semilogy(Eb_N0_dB, fer(3, :), 'b-s', 'LineWidth', 2, 'MarkerSize', 10, 'DisplayName', 'LDPC Soft'); % Linha Azul.
 xlabel('Eb/N0 (dB)');
 ylabel('FER');
 legend('Location', 'southwest');
 grid on;
 title('Desempenho do sistema BPSK com LDPC (FER)');
-
